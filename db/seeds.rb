0780f81c781a6e506c11a82fd3196f873540ced9
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-05 22:48:41 +0430

    Fix linters

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+User.create!([
+  {name: "Tom", photo: "https://unsplash.com/photos/F_-0BxGuVvo", bio: "Teacher from Mexico.", posts_counter: 0},
+  {name: "Lilly", photo: "https://unsplash.com/photos/F_-0BxGuVvo", bio: "Teacher from Poland.", posts_counter: 0}
+]),
