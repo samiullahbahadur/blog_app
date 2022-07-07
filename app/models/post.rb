f362d6a2f23af2855c56f9dd7861049cf359c192
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-07 12:11:03 +0430

    Remove user_id and count

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ApplicationRecordhas_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  def update_counter(count, user_id)
-    User.find(user_id).update(posts_counter: count)
+  def update_counter()
+    author.increment!(:posts_counter)
   end
 
   def recent_comments
