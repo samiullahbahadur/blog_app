86ffa962667a90fb8a03002b845a09f350b49901
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-07 10:26:08 +0430

    Fix lenters

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,17 @@ User.create!([
 ]),
 
 Post.create!([
-  {user_id: 1, title: 'Hello', text: 'This is my first post'}
-  {user_id: 2, title: 'Hello2', text: 'This is my 2nd post'}
-  {user_id: 1, title: 'Hello3', text: 'This is my 3rd post'}
-  {user_id: 2, title: 'Hello4', text: 'This is my 4th post'}
+  {users_id: 1, title: 'Hello', text: 'This is my first post'},
+  {users_id: 2, title: 'Hello2', text: 'This is my 2nd post'},
+  {users_id: 1, title: 'Hello3', text: 'This is my 3rd post'},
+  {users_id: 2, title: 'Hello4', text: 'This is my 4th post'}
 ]),
 
 Comment.create!([
-  {post_id: 1, user_id: 1, text: 'comment 1'}
-  {post_id: 1, user_id: 2, text: 'comment 2'}
-  {post_id: 1, user_id: 1, text: 'comment 3'}
-  {post_id: 1, user_id: 2, text: 'comment 4'}
-  {post_id: 1, user_id: 1, text: 'comment 5'}
-  {post_id: 1, user_id: 1, text: 'comment 6'}
+  {posts_id: 1, users_id: 1, text: 'comment 1'}
+  {posts_id: 1, users_id: 2, text: 'comment 2'}
+  {posts_id: 1, users_id: 1, text: 'comment 3'}
+  {posts_id: 1, users_id: 2, text: 'comment 4'}
+  {posts_id: 1, users_id: 1, text: 'comment 5'}
+  {posts_id: 1, users_id: 1, text: 'comment 6'}
 ])
