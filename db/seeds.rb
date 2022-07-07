328bc2e035b41cef66b53cafc7831b1fea7517ed
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-07 10:06:49 +0430

    Created fourth comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,5 @@ Comment.create!([
   {post_id: 1, user_id: 1, text: 'comment 1'}
   {post_id: 1, user_id: 2, text: 'comment 2'}
   {post_id: 1, user_id: 1, text: 'comment 3'}
+  {post_id: 1, user_id: 2, text: 'comment 4'}
 ])
