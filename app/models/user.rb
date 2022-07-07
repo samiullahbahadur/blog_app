f656c7f708358cebb05858a9401602da752442a8
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-07 10:47:14 +0430

    Implements all foreign_keys in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+
+    
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+
+
     def fetch_posts
         posts.order('created_at desc').limit(3)
       end
