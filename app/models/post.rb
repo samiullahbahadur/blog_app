fb6e80910ec31e5561c1aadfef6f8ef740c3b5e3
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-07 10:51:52 +0430

    Created update_counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ApplicationRecordhas_many :likes, dependent: :destroy
     has_many :comments, dependent: :destroy
     belongs_to :author, class_name: 'User', foreign_key: 'author_id'
     
-
+    def update_counter(count, user_id)
+        User.find(user_id).update(posts_counter: count)
+      end
 
 end  
