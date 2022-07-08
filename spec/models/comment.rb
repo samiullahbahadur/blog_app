0a98590d4382b7a16b4943f23ed9e5a0b9a0405d
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-08 12:08:31 +0430

    Add comment test

--- spec/models/comment.rb
+++ spec/models/comment.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  comment = Comment.new(text: 'This is my first comment')
+  before(:each) { comment.save }
+
+  context '#update_comments_counter' do
+    it 'should be private' do
+      expect { comment.update_comments_counter }.to raise_error(NoMethodError)
+    end
+  end
 end
