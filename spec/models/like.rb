85d1ed64109c00a060ebe98c1a39f330a10ece1e
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-08 12:09:10 +0430

    Add like  test

--- spec/models/like.rb
+++ spec/models/like.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  like = Like.new
+  before(:each) { like.save }
+
+  context '#update_likes_counter' do
+    it 'should be private' do
+      expect { like.update_likes_counter }.to raise_error(NoMethodError)
+    end
+  end
 end
