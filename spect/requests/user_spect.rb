8911b041f0187e197f7739a3ba5b301247f0eccd
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-08 04:53:19 +0430

    implement user spect test

--- spect/requests/user_spect.rb
+++ spect/requests/user_spect.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     context 'when the page is opened' do
       it 'returns a correct response' do
-        get '/user'
+        get '/users'
         expect(response).to have_http_status(200)
         expect(response.body).to include('<h1>Users</h1>')
         expect(response.body).to include('<p>Show all users</p>')
@@ -16,11 +16,11 @@ RSpec.describe 'Users', type: :request do
   describe 'GET /show' do
     context 'when the page is opened' do
       it 'returns a correct response' do
-        get '/user/1'
+        get '/users/1'
         expect(response).to have_http_status(200)
         expect(response.body).to include('<h1>Users</h1>')
         expect(response.body).to include('<p>Show user details</p>')
-        expect(response.body).to render_template('user/show')
+        expect(response.body).to render_template('users/show')
       end
     end
   end
