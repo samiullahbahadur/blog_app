2ea9ff1015d6daa3bfaab45ad5c08b4a8c014df4
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-13 09:37:10 +0430

    Create post_request_spec file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @user=User.all
   end
 
   def show
-    @user = User.find(params[:id])
+  @user=User.find(params[:id])
+  @post=Post.where(author_id:@user.id).order(created_at: :desc)
   end
 end
