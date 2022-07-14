89de26d9d4fec4af0237239c55658341ef40b59d
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-14 22:12:39 +0430

    Fix linters

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,11 +19,10 @@
 #   {posts_id: 1, users_id: 1, text: 'comment 6'}
 # ])
 
-
 # User.create!([
-              #  { name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',posts_counter: 0 },
-              #  { name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0 }
-              # ])
+#  { name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',posts_counter: 0 },
+#  { name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0 }
+# ])
 
 # Post.create!([
 #                { author_id: 1, title: 'Hello', text: 'This is my first post' },
@@ -31,11 +30,11 @@
 #                { author_id: 1, title: 'Hello3', text: 'This is my 3rd post' },
 #                { author_id: 2, title: 'Hello4', text: 'This is my 4th post' }
 #              ])
-             Post.create!([
-                        {author_id: 1, title: 'Hello', text: 'This is my first post'},
-                        {author_id: 2, title: 'Hello2', text: 'This is my 2nd post'},
-                        {author_id: 1, title: 'Hello3', text: 'This is my 3rd post'},
-                        {author_id: 2, title: 'Hello4', text: 'This is my 4th post'}
+Post.create!([
+               { author_id: 1, title: 'Hello', text: 'This is my first post' },
+               { author_id: 2, title: 'Hello2', text: 'This is my 2nd post' },
+               { author_id: 1, title: 'Hello3', text: 'This is my 3rd post' },
+               { author_id: 2, title: 'Hello4', text: 'This is my 4th post' }
              ])
 
 # Comment.create!([
@@ -47,11 +46,11 @@
 #                   { post_id: 1, author_id: 1, text: 'comment 6' }
 #                 ])
 
-                Comment.create!([
-                          {posts_id: 1, users_id: 1, text: 'comment 1'},
-                          {posts_id: 1, users_id: 2, text: 'comment 2'},
-                          {posts_id: 1, users_id: 1, text: 'comment 3'},
-                          {posts_id: 1, users_id: 2, text: 'comment 4'},
-                          {posts_id: 1, users_id: 1, text: 'comment 5'},
-                          {posts_id: 1, users_id: 1, text: 'comment 6'}
-                   ])
+Comment.create!([
+                  { posts_id: 1, users_id: 1, text: 'comment 1' },
+                  { posts_id: 1, users_id: 2, text: 'comment 2' },
+                  { posts_id: 1, users_id: 1, text: 'comment 3' },
+                  { posts_id: 1, users_id: 2, text: 'comment 4' },
+                  { posts_id: 1, users_id: 1, text: 'comment 5' },
+                  { posts_id: 1, users_id: 1, text: 'comment 6' }
+                ])
