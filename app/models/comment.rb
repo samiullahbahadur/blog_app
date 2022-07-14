081ca526c6c82343cdb8f80bb8b3080696aad90e
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-14 20:53:46 +0430

    Add div container for users index

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :author
+  belongs_to :user, foreign_key: 'author_id'
   def update_comments_counter_for_post
     post.update(comments_counter: post.comments.count)
   end
