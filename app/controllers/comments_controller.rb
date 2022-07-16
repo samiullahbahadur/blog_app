5c0e07d79d06fe166450f1c87ff09a69c46eb98b
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-16 16:12:42 +0430

    Refactor migrations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,12 @@
 class CommentsController < ApplicationController
-  def index
+  def new
+    @comment=Comment.new
+  end
+  def create 
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = @user.comments.new(post: @post, text: params[:text])
+    @comment.save
+    redirect_to user_post_path(@user, @post)
   end
 end
