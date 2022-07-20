2e77a0c1f129324e6d2a1d163a4a690a00099e99
samiullahbahadur <bahadursamiullah@gmail.com>
2022-07-20 18:23:15 +0430

    Add styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirects signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
   devise_for :users
   root 'users#index'
   resources :users, only: [:index, :show] do
