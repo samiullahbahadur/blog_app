48aebb5bf896e274c5bb437c37adc4a9ebf18f83
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-22 16:25:08 +0430

    Fix post_counters

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,12 +10,13 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :update_counters
-  after_destroy :update_counters
+  after_destroy :strdeoy_update_counters
+
   def update_counters
     user.increment!(:posts_counter)
   end
 
-  def update_counters
+  def strdeoy_update_counters
     user.decrement!(:posts_counter)
   end
 
